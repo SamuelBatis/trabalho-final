78de27d51f23f667622a2627f6c2a17a523f42cb
Samuel <samuel.batistela12@gmail.com>
2022-06-16 08:37:04 -0300

    fix: valores retornados da funcao

--- final.c
+++ final.c
@@ -2,23 +2,23 @@
 #include <math.h>
 #include <string.h>
 
-float r (t) {
+float r (float t) {
     float result = 300*exp(0.3*t);
   return result;
 };
 float trapezio (int a,int b) {
     int xn = b; 
     int xo = a;   
-    int n = 500;
+    int n = 5;
     float h = (xn - xo) / n;
     float x = xo + h;
-    int soma = 0;
+    float soma = 0;
     for (int i = 1; i != n ; i++) {
       soma += r(x);
       x += h;
     }
     float p = h * ((r(xo) + r(xn)) / 2 + soma);
-    printf("\nsoma %f\n", p);
+    printf("h %f\n", h);
   return p;
 };
 
@@ -43,7 +43,7 @@ int main(void)
   int i;
 
   // input dos dados
-  for (i = 0; i < n; i++)
+  for (int i = 0; i < n; i++)
   {
     printf("\ninsira o nome: ");
     scanf("\n%s", &pesquisadores[i].nome);
@@ -53,12 +53,23 @@ int main(void)
     scanf("\n%d", &pesquisadores[i].a);
     printf("\ninsira o valor de b: ");
     scanf("\n%d", &pesquisadores[i].b);
-    printf("\ninsira o r: ");
-    scanf("\n%f", &pesquisadores[i].r);
+
   };
-  printf("a = %d e b = %d", pesquisadores[0].a, pesquisadores[0].b);
-  int l = trapezio(pesquisadores[0].a, pesquisadores[0].b);
-  printf("valor retornado do trapezio: %f\n", l);
+
+  float r = trapezio(2, 3);
+  printf("r = %f\n", r);
+  
+
+
+  // chamada das funçoes
+for (int k = 0; k < n; k++) {
+    printf("A e B dentro do for %d, %d\n", pesquisadores[k].a, pesquisadores[k].b);
+    float aux = trapezio(pesquisadores[k].a, pesquisadores[k].b);
+    printf("%f", aux);
+    pesquisadores[k].r = aux;
+}
+  printf("a = %d e b = %d\n", pesquisadores[0].a, pesquisadores[0].b);
+  printf("valor retornado do trapezio: %f\n", pesquisadores[0].r);
   
   printf("pesquisadores\n");
   for (int k = 0; k < n; k++)
