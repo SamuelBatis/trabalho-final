bc0f091ccc5e219cf62246008c61f790f015fb42
Samuel <samuel.batistela12@gmail.com>
2022-06-18 11:10:39 -0300

    refactor: function do trapezio

--- final.c
+++ final.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
 
-float r (float t) {
-    float result = 300*exp(0.3*t);
+float r(float t)
+{
+  float result = 300 * exp(0.3 * t);
+  // printf("%f\n", result);
   return result;
 };
-float trapezio (int a,int b) {
-    int xn = b; 
-    int xo = a;   
-    int n = 5;
-    float h = (xn - xo) / n;
-    float x = xo + h;
-    float soma = 0;
-    for (int i = 1; i != n ; i++) {
+float trapezio(int a, int b, int num)
+{
+  int xn = b;
+  int xo = a;
+  int n = num;
+  float p;
+  float h = (xn - xo) / n;
+  float x = xo + h;
+  float soma = 0;
+  for (int i = 1; i != n; i++)
+  {
       soma += r(x);
       x += h;
     }
-    float p = h * ((r(xo) + r(xn)) / 2 + soma);
-    printf("h %f\n", h);
+  p = h * ((r(xo) + r(xn)) / 2 + soma);
+  printf("h %f\n", h);
   return p;
 };
 
-
-
-int main(void)
+int main()
 {
-  // char nome[45];
   int n;
   scanf("%d", &n);
 
@@ -36,45 +39,46 @@ int main(void)
     int periodo;
     int a;
     int b;
-    float r;
+    double r;
   };
 
   struct Pesquisadores pesquisadores[n];
-  int i;
 
   // input dos dados
   for (int i = 0; i < n; i++)
   {
+    pesquisadores[i].periodo = i;
     printf("\ninsira o nome: ");
     scanf("\n%s", &pesquisadores[i].nome);
-    printf("\ninsira o periodo: ");
-    scanf("\n%d", &pesquisadores[i].periodo);
     printf("\ninsira o valor de a: ");
     scanf("\n%d", &pesquisadores[i].a);
     printf("\ninsira o valor de b: ");
     scanf("\n%d", &pesquisadores[i].b);
-
   };
 
-  float r = trapezio(2, 3);
-  printf("r = %f\n", r);
-  
-
-
   // chamada das funçoes
-for (int k = 0; k < n; k++) {
-    printf("A e B dentro do for %d, %d\n", pesquisadores[k].a, pesquisadores[k].b);
-    float aux = trapezio(pesquisadores[k].a, pesquisadores[k].b);
-    printf("%f", aux);
-    pesquisadores[k].r = aux;
-}
-  printf("a = %d e b = %d\n", pesquisadores[0].a, pesquisadores[0].b);
-  printf("valor retornado do trapezio: %f\n", pesquisadores[0].r);
-  
-  printf("pesquisadores\n");
   for (int k = 0; k < n; k++)
   {
-    printf("\n%d nome: %s, periodo: %d,  valor de a: %d, valor de b: %d, valor de r: %f\n", k + 1, pesquisadores[k].nome, pesquisadores[k].periodo, pesquisadores[k].a, pesquisadores[k].b, pesquisadores[k].r);
+    printf("A e B dentro do for %d, %d\n", pesquisadores[k].a, pesquisadores[k].b);
+    float aux = trapezio(pesquisadores[k].a, pesquisadores[k].b, n);
+    printf(" ok %f", aux);
+    pesquisadores[k].r = aux;
+  }
+  printf("\na = %d e b = %d\n", pesquisadores[0].a, pesquisadores[0].b);
+  printf("valor retornado do trapezio: %.2f\n", pesquisadores[0].r);
+
+  // output
+  printf("pesquisadores %d\n", n);
+  printf("periodo   nome do pesquisador  Intervalo[a,b]  Valor proporcional");
+  for (int k = 0; k < n; k++)
+  {
+    double rr = pesquisadores[k].r + 1200;
+    printf("\n%d          %s                   [%d, %d]           %.2f\n",
+           k + 1,
+           pesquisadores[k].nome,
+           pesquisadores[k].a,
+           pesquisadores[k].b,
+           rr);
   }
 
   return 0;
